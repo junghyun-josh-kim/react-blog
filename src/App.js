e0c711b92e77b94a1a298b2b3e86085b826936a4
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-10-06 22:07:13 -0400

    React: Structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [title, setTitle] = useState(['bus', 'apple', 'trian']);
+  let [like, setLike] = useState(0);
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav-bar'>
+        <h4 style={{ fontSize: '22px' }}>Maple Tech</h4>
+      </div>
+      <button
+        onClick={() => {
+          let titleSort = [...title];
+          titleSort.sort();
+          setTitle(titleSort);
+        }}
+      >
+        ABCD...
+      </button>
+      <button
+        onClick={() => {
+          let newTitle = [...title];
+          newTitle[0] = 'TITLE1';
+          setTitle(newTitle);
+        }}
+      >
+        Today's Recommendation
+      </button>
+      <div className='list'>
+        <h4>
+          {title[0]}{' '}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+          >
+            👍
+          </span>{' '}
+          {like}
+        </h4>
+        <p>Oct 6, 2022</p>
+      </div>
+      <div className='list'>
+        <h4>{title[1]}</h4>
+        <p>Oct 6, 2022</p>
+      </div>
+      <div className='list'>
+        <h4>{title[2]}</h4>
+        <p>Oct 6, 2022</p>
+      </div>
     </div>
   );
 }
